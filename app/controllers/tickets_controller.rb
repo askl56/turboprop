20f9fc644d948896518e654871d5ce402e7bfbda
askl56 <amscott@openmailbox.org>
2015-10-17 19:43:40 +0100

    Implement deleting tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -33,6 +33,12 @@ class TicketsController < ApplicationController
   def edit
   end
 
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+
   private
 
   def set_ticket
