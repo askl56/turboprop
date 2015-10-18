19acc57b4831b379f3aae8c0afeda76db9ba42f7
askl56 <amscott@openmailbox.org>
2015-10-18 15:18:42 +0100

    Only admins can see the links to create and delete projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+
   def title(*parts)
     unless parts.empty?
       content_for :title do
         (parts << "Turboprop").join(" - ")
       end
     end
+  end
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
+
+  def authorized?(permission, thing, &block)
+    block.call if can?(permission.to_sym, thing) ||
+      current_user.try(:admin?)
   end
 end
