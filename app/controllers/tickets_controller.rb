b6517a1c8bdc570dc9e9ab3e607c33f04e1c70ea
askl56 <amscott@openmailbox.org>
2015-10-24 19:33:28 +0100

    Only members of a project can view tickets in it

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -30,6 +30,7 @@ class TicketsController < ApplicationController
   end
 
   def show
+    authorize @ticket, :show?
   end
 
   def edit
--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -6,39 +6,48 @@ describe ProjectPolicy do
 
   subject { ProjectPolicy }
 
-  permissions :show? do
-    let(:user) { FactoryGirl.create :user }
+  context "permissions" do
+    subject { ProjectPolicy.new(user, project) }
 
-    let(:project) { FactoryGirl.create :project }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
 
-    it "blocks anonymous users" do
-      expect(subject).not_to permit(nil, project)
+    context "for anonymous users" do
+      let(:user) { nil }
+      it { should_not permit_action :show }
+      it { should_not permit_action :update }
+    end
+    context "for viewers of the project" do
+      before { assign_role!(user, :viewer, project) }
+
+      it { should permit_action :show }
+      it { should_not permit_action :update }
     end
 
-    it "allows viewers of the project" do
-      assign_role!(user, :viewer, project)
-      expect(subject).to permit(user, project)
+    context "for editors of the project" do
+      before { assign_role!(user, :editor, project) }
+      it { should permit_action :show }
+      it { should_not permit_action :update }
     end
 
-    it "allows editors of the project" do
-      assign_role!(user, :editor, project)
-      expect(subject).to permit(user, project)
+    context "for managers of the project" do
+      before { assign_role!(user, :manager, project) }
+      it { should permit_action :show }
+      it { should permit_action :update }
     end
 
-    it "allows managers of the project" do
-      assign_role!(user, :manager, project)
-      expect(subject).to permit(user, project)
+    context "for managers of other projects" do
+      before do
+        assign_role!(user, :manager, FactoryGirl.create(:project))
+      end
+      it { should_not permit_action :show }
+      it { should_not permit_action :update }
     end
 
-    it "allows administrators" do
-      admin = FactoryGirl.create :user, :admin
-      expect(subject).to permit(admin, project)
-    end
-
-    it "doesn't allow users assigned to other projects" do
-      other_project = FactoryGirl.create :project
-      assign_role!(user, :manager, other_project)
-      expect(subject).not_to permit(user, project)
+    context "for administrators" do
+      let(:user) { FactoryGirl.create :user, :admin }
+      it { should permit_action :show }
+      it { should permit_action :update }
     end
   end
 
@@ -64,41 +73,6 @@ describe ProjectPolicy do
     it "returns all projects for admins" do
       user.admin = true
       expect(subject).to include(project)
-    end
-  end
-
-  permissions :update? do
-    let(:user) { FactoryGirl.create :user }
-    let(:project) { FactoryGirl.create :project }
-
-    it "blocks anonymous users" do
-      expect(subject).not_to permit(nil, project)
-    end
-
-    it "doesn't allow viewers of the project" do
-      assign_role!(user, :viewer, project)
-      expect(subject).not_to permit(user, project)
-    end
-
-    it "doesn't allows editors of the project" do
-      assign_role!(user, :editor, project)
-      expect(subject).not_to permit(user, project)
-    end
-
-    it "allows managers of the project" do
-      assign_role!(user, :manager, project)
-      expect(subject).to permit(user, project)
-    end
-
-    it "allows administrators" do
-      admin = FactoryGirl.create :user, :admin
-      expect(subject).to permit(admin, project)
-    end
-
-    it "doesn't allow users assigned to other projects" do
-      other_project = FactoryGirl.create :project
-      assign_role!(user, :manager, other_project)
-      expect(subject).not_to permit(user, project)
     end
   end
 end
