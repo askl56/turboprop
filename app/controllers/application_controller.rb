4925644fa6ec3ba7274e480f482858e8b346b5c6
askl56 <amscott@openmailbox.org>
2015-10-24 19:07:54 +0100

    Handle authorization errors gracefully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    redirect_to root_path, alert: "You aren't allowed to do that."
+  end
 end
