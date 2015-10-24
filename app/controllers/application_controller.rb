c446f18ec36f6e85bde15102c315c98d7da0510a
askl56 <amscott@openmailbox.org>
2015-10-24 20:04:49 +0100

    Verify that we use authorization in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
+
   protect_from_forgery with: :exception
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
