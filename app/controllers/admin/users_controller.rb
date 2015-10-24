1750fc6024a8b4ead2af0797219e79a3fc8ced57
askl56 <amscott@openmailbox.org>
2015-10-24 14:17:22 +0100

    added the ability for admins to edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,15 @@
 class Admin::UsersController < Admin::ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.order(:email)
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def new
@@ -18,9 +27,26 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been updated."
+      render "edit"
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
