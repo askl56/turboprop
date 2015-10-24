efc0517409a510f7cd1d79077475a681c399331a
askl56 <amscott@openmailbox.org>
2015-10-24 20:12:59 +0100

    Admins can edit the project roles of existing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < Admin::ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :archive]
+  before_action :set_projects, only: [:new, :create, :edit, :update]
 
   def index
     @users = User.excluding_archived.order(:email)
@@ -31,12 +32,22 @@ class Admin::UsersController < Admin::ApplicationController
     if params[:user][:password].blank?
       params[:user].delete(:password)
     end
-    if @user.update(user_params)
-      flash[:notice] = "User has been updated."
-      redirect_to admin_users_path
-    else
-      flash.now[:alert] = "User has not been updated."
-      render "edit"
+    User.transaction do
+      @user.roles.clear
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+        end
+      end
+      if @user.update(user_params)
+        flash[:notice] = "User has been updated."
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
@@ -60,5 +71,9 @@ class Admin::UsersController < Admin::ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def set_projects
+    @projects = Project.order(:name)
   end
 end
