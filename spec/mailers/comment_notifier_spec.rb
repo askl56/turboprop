cab5f6cdc67d794cc1d771eecb3de00698a760fc
askl56 <amscott@openmailbox.org>
2015-11-08 17:01:13 +0000

    Automatically subscribe users to a ticket when they comment on it

--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -1,21 +1,21 @@
 require "rails_helper"
 
 RSpec.describe CommentNotifier, type: :mailer do
+
   describe "created" do
+
     let(:project) { FactoryGirl.create(:project) }
     let(:ticket_owner) { FactoryGirl.create(:user) }
+
     let(:ticket) do
-      FactoryGirl.create(:ticket,
-                         project: project, author: ticket_owner)
+      FactoryGirl.create(:ticket, project: project, author: ticket_owner)
     end
+
     let(:commenter) { FactoryGirl.create(:user) }
-    let(:comment) do
-      Comment.new(ticket: ticket, author: commenter,
-                  text: "Test comment")
-    end
-    let(:email) do
-      CommentNotifier.created(comment, ticket_owner)
-    end
+
+    let(:comment) { Comment.new(ticket: ticket, author: commenter, text: "Test comment") }
+
+    let(:email) { CommentNotifier.created(comment, ticket_owner) }
 
     it "sends out an email notification about a new comment" do
       expect(email.to).to include ticket_owner.email
