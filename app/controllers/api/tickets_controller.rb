daf1ff3955e5acabbe0d62f8f5f595e95ddbd292
askl56 <amscott@openmailbox.org>
2015-11-08 18:12:27 +0000

    Felshed out ticketsAPI with creating tickets and responses for errors conditions

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,9 +1,6 @@
-class API::TicketsController < ApplicationController
-
-  attr_reader :current_user
+class API::TicketsController < API::ApplicationController
 
   before_action :set_project
-  before_action :authenticate_user
 
   def show
     @ticket = @project.tickets.find(params[:id])
@@ -11,15 +8,24 @@ class API::TicketsController < ApplicationController
     render json: @ticket
   end
 
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    authorize @ticket, :create?
+
+    if @ticket.save
+      render json: @ticket, status: 201
+    else
+      render json: { errors: @ticket.errors.full_messages }, status: 422
+    end
+  end
+
   private
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :description)
+  end
 
   def set_project
     @project = Project.find(params[:project_id])
-  end
-
-  def authenticate_user
-    authenticate_with_http_token do |token|
-      @current_user = User.find_by(api_key: token)
-    end
   end
 end
